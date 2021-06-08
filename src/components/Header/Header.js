244586a9023fa44f7f6a493b80f7473cb1c58353
Delyana Yordanova <delyana.yordanova@outlook.com>
2021-06-08 17:20:52 +0300

    css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -92,7 +92,7 @@ const Header = ({
           expand="md"
           fixed="top"
           scrolling
-          style={{ backgroundColor: `${content === 'account' ? '#42414094' : '#f7f7f7'}` }}
+          style={{ backgroundColor: `${content === 'account' ? '#787877' : '#f7f7f7'}` }}
         >
           <MDBNavbarNav right style={specialCaseNavbarStyles}>
             {getUser().role === 'employee' && (
